8b5214bade5b594cd1fd76941716498c8e5a7eba
KhaledHamouda <khaled7amoda@gmail.com>
2024-08-07 19:48:50 +0300

    Setup backend and frontend environments.

---
+++ Backend/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const dotenv = require('dotenv').config()
+// connectDb = require('./config/dbConnection')
+
+// Mongoose connection
+//connectDb()
+
+const app = express();
+
+// Api routes and middlewares.
+
+app.use(express.json());
+
+//running the server
+const port = process.env.PORT || 3030
+app.listen(port, ()=>{
+    console.log(`server is running on port ${port}`);
+})
