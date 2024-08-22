06b787f252fde079ddfcde2eec0611392318ebbd
KhaledHamouda <khaled7amoda@gmail.com>
2024-08-23 01:54:47 +0300

    Solving servers down bugs.

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -19,16 +19,10 @@ import CategoryList from "./components/Category/CategoryList";
 import Dashboard from "./pages/Dashboard";
 import LoginForm from "./pages/Login";
 import SignUp from "./pages/SignUp";
-<<<<<<< HEAD
+import BookDetail from "./components/Book/BookDetail";
+import CategoryDetail from "./components/Category/CategoryDetail";
+import AuthorDetail from "./components/Author/AuthorDetail";
 
-=======
-import BookDetail from "./components/Book/BookDetail";
-import CategoryDetail from "./components/Category/CategoryDetail";
-import AuthorDetail from "./components/Author/AuthorDetail";
->>>>>>> fe04995 ( add categories pages)
-import BookDetail from "./components/Book/BookDetail";
-import CategoryDetail from "./components/Category/CategoryDetail";
-import AuthorDetail from "./components/Author/AuthorDetail";
 
 function App() {
   const [isAdmin, setIsAdmin] = useState<boolean>(() => {
