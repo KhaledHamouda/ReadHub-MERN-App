30a71b7a50f8ae4bf9e1f51e815e9145833f1871
khalidhamdi <khalidhamdi204@gmail.com>
2024-08-23 00:10:14 +0300

    improve Book Pages style

--- Frontend/src/components/Dashboard/UsersTable.tsx
+++ Frontend/src/components/Dashboard/UsersTable.tsx
@@ -64,6 +64,53 @@ const UsersTable: React.FC = () => {
     fetchUserBooks();
   }, []);
 
+  const handleStateChange = async (bookId: string, newState: string) => {
+    const token = sessionStorage.getItem("userToken");
+
+    if (!token) {
+      alert("Please log in to change your book state.");
+      return;
+    }
+
+    let userId: string | null = null;
+
+    try {
+      const decoded: DecodedToken = jwtDecode(token);
+      userId = decoded.id;
+    } catch (error) {
+      console.error("Error decoding token:", error);
+    }
+
+    if (!userId) {
+      alert("Unable to determine user ID.");
+      return;
+    }
+
+    try {
+      await axiosInstance.post(
+        `/userbook/state`,
+        {
+          userId,
+          bookId,
+          state: newState,
+        },
+        { headers: { Authorization: `Bearer ${token}` } }
+      );
+
+      setUserBooks((prevBooks) =>
+        prevBooks.map((userBook) =>
+          userBook.bookId && userBook.bookId._id === bookId
+            ? { ...userBook, state: newState }
+            : userBook
+        )
+      );
+
+      alert("State updated successfully!");
+    } catch (error) {
+      console.error("Failed to update book state", error);
+    }
+  };
+
   const handleRatingChange = async (bookId: string, newRating: number) => {
     const token = sessionStorage.getItem("userToken");
 
@@ -143,7 +190,18 @@ const UsersTable: React.FC = () => {
               <TableCell>
                 {userBook.bookId ? userBook.bookId.title : "No Title"}
               </TableCell>
-              <TableCell>{userBook.state}</TableCell>
+              <TableCell>
+                <select
+                  value={userBook.state}
+                  onChange={(e) =>
+                    handleStateChange(userBook.bookId!._id, e.target.value)
+                  }
+                >
+                  <option value="Want to Read">Want to Read</option>
+                  <option value="Currently Reading">Currently Reading</option>
+                  <option value="Read">Read</option>
+                </select>
+              </TableCell>
               <TableCell>
                 {userBook.bookId &&
                   [1, 2, 3, 4, 5].map((star) => (
